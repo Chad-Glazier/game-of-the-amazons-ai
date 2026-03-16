bea61fe6378d808d83e0faa77f8f052d3171e20f
Chad-Glazier <chadglazier@outlook.com>
2026-03-16 16:38:34 -0700

    corrected encoding

--- src/main/java/ubc/cosc322/player/Util.java
+++ src/main/java/ubc/cosc322/player/Util.java
@@ -71,11 +71,11 @@ public class Util {
 				switch (board.get(i * 11 + j)) {
 				case 0:
 					continue;
-				case 1:
+				case 2:
 					queens[whiteIdx++] = position;
 					BitBoard.flag(occupancy, position);
 					break;
-				case 2:
+				case 1:
 					queens[blackIdx++] = position;
 					BitBoard.flag(occupancy, position);
 					break;
